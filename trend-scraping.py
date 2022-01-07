81a5cb3cc3443cc49f0d34374424b9ca23292a4a
Marilzon <maxmaril@hotmail.com>
2022-01-07 10:43:42 -0300

    callback to list second pagination ranking

--- trend-scraping.py
+++ trend-scraping.py
@@ -1,15 +1,19 @@
 import scrapy
 
 class TopTokens(scrapy.Spider):
-  name = 'TopTokens'
+  name = 'top40tokens'
   start_urls = ['https://beincrypto.com.br/converter']
 
   def parse(self, response):
     trend_tokens = response.css('p>strong::text').getall()
-    number = 1
-    for token in trend_tokens:
-      yield {
-        number: token,
-      }
-      number += 1
 
+    for token in trend_tokens:
+      name = 'id'
+      yield {
+        name: token,
+      }
+
+    next_page = response.xpath('//a[contains(@href, "/converter/2")]/@href').get()
+
+    if next_page is not None:
+      yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
